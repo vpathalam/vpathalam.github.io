df41a1c2e6629230629f188d7cb6c79f285ea2fb
vpathalam <vpathalam@gmail.com>
2023-04-16 14:49:34 -0400

    Updates

--- README.md
+++
@@ -1,2 +0,0 @@
-Currently Developing Personal Website : Work In Progress, Version 2. © Vikram Pathalam, 2020.
-
---
+++ precache-manifest.465324b0d2b7ee20987395533e1fbf9a.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a60d60d882dae3eec284a76d97c284d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f273329e780cb4123177",
+    "url": "/static/css/2.16579b8c.chunk.css"
+  },
+  {
+    "revision": "12351fb3439ef2c62e28",
+    "url": "/static/css/main.caacc6f3.chunk.css"
+  },
+  {
+    "revision": "f273329e780cb4123177",
+    "url": "/static/js/2.741843ca.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.741843ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12351fb3439ef2c62e28",
+    "url": "/static/js/main.3e42c491.chunk.js"
+  },
+  {
+    "revision": "840cad3f0cb1ae20fd2c",
+    "url": "/static/js/runtime-main.a52c8626.js"
+  },
+  {
+    "revision": "e3934e639f707e0aaef80b9706a4370c",
+    "url": "/static/media/avatar.e3934e63.png"
+  },
+  {
+    "revision": "e269360adad4e273c5778af410671b0e",
+    "url": "/static/media/carnegie.e269360a.jpg"
+  },
+  {
+    "revision": "ec4cf1a657ab7b6bb305e72baad0fd69",
+    "url": "/static/media/graham.ec4cf1a6.jpg"
+  },
+  {
+    "revision": "2876a08cc7bb7c0972dfc86158013ce3",
+    "url": "/static/media/hanh.2876a08c.jpg"
+  },
+  {
+    "revision": "8d5fde514e30e396a1392b732ca6649b",
+    "url": "/static/media/hawking.8d5fde51.jpg"
+  },
+  {
+    "revision": "49ef5497d71eb296f7b72fd3c4bf27ab",
+    "url": "/static/media/krishnamurthy.49ef5497.jpg"
+  },
+  {
+    "revision": "7a33d6a7c4971af22c65fcdeb8b80899",
+    "url": "/static/media/kwik.7a33d6a7.jpg"
+  },
+  {
+    "revision": "99a23526d0385f0e1b3b65b497db42f3",
+    "url": "/static/media/logo.99a23526.png"
+  },
+  {
+    "revision": "bf23289d8054dbd104d73739c69d5afd",
+    "url": "/static/media/northeastern.bf23289d.png"
+  },
+  {
+    "revision": "118f9b3d7d8ed0a25adac9b07ce5e1e2",
+    "url": "/static/media/thiel.118f9b3d.jpg"
+  },
+  {
+    "revision": "dcd0a85ac050d8cf9d105acfd8dcca53",
+    "url": "/static/media/tolle.dcd0a85a.jpg"
+  }
+]);
